4bde882b786e8d2f0d34e6608aeb0ef250cee514
Michał Hanusek <gobler@myfreak.pl>
2023-10-18 20:12:51 +0200

    first article

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,15 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Gobler - blog',
+    title: 'Gobler\'s blog',
     description: 'The minimalist blog theme',
+
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
       width: 400,
       height: 300
     },
+
     header: {
       position: 'left', // possible value are : | 'left' | 'center' | 'right'
       logo: {
@@ -16,20 +18,22 @@ export default defineAppConfig({
         alt: 'Gobler\'s Blog' // alt of the logo
       }
     },
+
     footer: {
       credits: {
         // possible value are : true | false
         enabled: false,
 
         // our github repository
-        repository: 'https://github.com/Gobler/Personal-blog',
+        repository: '',
 
         text: ''
       },
       navigation: false, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: '' // string that will be displayed in the footer (leave empty or delete to disable)
     },
+
     socials: {
       twitter: '',
       instagram: 'gobl3r',
@@ -42,8 +46,13 @@ export default defineAppConfig({
 
       github: 'Gobler'
     },
+
     form: {
       successMessage: 'Message sent. Thank you!'
+    },
+
+    backToTop: {
+      text: 'Do góry'
     }
   }
 })
